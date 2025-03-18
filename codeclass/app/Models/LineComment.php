e7970876997f3f5275a5578fa4e02828029eb4f5
tawbazf <tawbazehaf@gmail.com>
2025-03-18 11:46:18 +0100

    line comment model

--- codeclass/app/Models/LineComment.php
+++ codeclass/app/Models/LineComment.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LineComment extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'code_file_id',
+        'line_number',
+        'content',
+        'category', // 'functionality', 'performance', 'style'
+    ];
+
+    /**
+     * Get the user that owns the line comment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the code file that owns the line comment.
+     */
+    public function codeFile()
+    {
+        return $this->belongsTo(CodeFile::class);
+    }
 }
