50ea25bfe06e3a77e8cad355cea084800fecaa5f
tawbazf <tawbazehaf@gmail.com>
2025-03-18 11:47:16 +0100

    assignment  model

--- codeclass/app/Models/Assignment.php
+++ codeclass/app/Models/Assignment.php
@@ -2,9 +2,48 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Assignment extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'created_by', // user_id of the teacher
+        'status', // 'draft', 'published', 'archived'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
+
+    /**
+     * Get the teacher who created the assignment.
+     */
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * Get the code submissions for this assignment.
+     */
+    public function codeSubmissions()
+    {
+        return $this->hasMany(CodeSubmission::class);
+    }
 }
