1fbe798213d548f9ad1174725bfcfa4df0d8ffca
tawbazf <tawbazehaf@gmail.com>
2025-03-18 11:45:30 +0100

    comment model

--- codeclass/app/Models/Comment.php
+++ codeclass/app/Models/Comment.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'code_submission_id',
+        'content',
+        'category', // 'functionality', 'performance', 'style'
+    ];
+
+    /**
+     * Get the user that owns the comment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the code submission that owns the comment.
+     */
+    public function codeSubmission()
+    {
+        return $this->belongsTo(CodeSubmission::class);
+    }
 }
