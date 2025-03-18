134b9eb4bfd07fb588ed0e662e23ef5849bf05ff
tawbazf <tawbazehaf@gmail.com>
2025-03-18 11:54:27 +0100

    adding routes for register and login

--- codeclass/routes/web.php
+++ codeclass/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 });
