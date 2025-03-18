5e7301e5ecdadd9e6c39bd38d525ddaf50040990
tawbazf <tawbazehaf@gmail.com>
2025-03-18 11:46:45 +0100

    badge  model

--- codeclass/app/Models/Badge.php
+++ codeclass/app/Models/Badge.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'image_path',
+        'category', // 'code_review', 'participation', 'achievement'
+        'level', // 'beginner', 'intermediate', 'advanced'
+        'points_required',
+    ];
+
+    /**
+     * Get the users that have earned this badge.
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_badges')
+            ->withPivot('awarded_at')
+            ->withTimestamps();
+    }
 }
