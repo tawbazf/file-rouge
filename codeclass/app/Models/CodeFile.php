e9c3c73cacd2d032e3d221a8bc7f69923041558c
tawbazf <tawbazehaf@gmail.com>
2025-03-18 11:44:31 +0100

    code file model

--- codeclass/app/Models/CodeFile.php
+++ codeclass/app/Models/CodeFile.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CodeFile extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'code_submission_id',
+        'filename',
+        'file_path',
+        'content',
+        'language', // 'javascript', 'php', 'python', etc.
+    ];
+
+    /**
+     * Get the code submission that owns the file.
+     */
+    public function codeSubmission()
+    {
+        return $this->belongsTo(CodeSubmission::class);
+    }
+
+    /**
+     * Get the line-specific comments for this file.
+     */
+    public function lineComments()
+    {
+        return $this->hasMany(LineComment::class);
+    }
 }
