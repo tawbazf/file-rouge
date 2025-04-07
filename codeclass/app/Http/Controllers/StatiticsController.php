f26a19ab033ba2b294841b00ef588e1887d2c53a
tawbazf <tawbazehaf@gmail.com>
2025-04-07 14:31:32 +0100

    statics controller

--- codeclass/app/Http/Controllers/StatiticsController.php
+++ codeclass/app/Http/Controllers/StatiticsController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class StatiticsController extends Controller
+class StatisticsController extends Controller
 {
-    //
+    public function index()
+    {
+        // Exemple de données pour les statistiques générales
+        $classAverage = 15.8;
+        $activeStudents = 28;
+        $totalStudents = 30;
+        $homeworkCompletion = 92; // En pourcentage
+        $globalProgress = 78; // En pourcentage
+
+        // Liste des étudiants avec leurs informations
+        $students = [
+            [
+                'id' => '2025001',
+                'name' => 'Emma Bernard',
+                'avatar' => 'https://randomuser.me/api/portraits/women/32.jpg',
+                'average' => 17.5,
+                'progress' => 85,
+                'lastActivity' => 'Il y a 2 heures',
+            ],
+            [
+                'id' => '2025002',
+                'name' => 'Lucas Martin',
+                'avatar' => 'https://randomuser.me/api/portraits/men/45.jpg',
+                'average' => 14.8,
+                'progress' => 70,
+                'lastActivity' => 'Hier',
+            ],
+        ];
+
+        // Passer les données à la vue
+        return view('general-statistics', compact(
+            'classAverage',
+            'activeStudents',
+            'totalStudents',
+            'homeworkCompletion',
+            'globalProgress',
+            'students'
+        ));
+    }
 }
