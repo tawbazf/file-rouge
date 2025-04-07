38b05abf382c8c07940393931f50c53e1be0c05f
tawbazf <tawbazehaf@gmail.com>
2025-04-07 10:52:52 +0100

    adding dashboard routes

--- codeclass/routes/web.php
+++ codeclass/routes/web.php
@@ -3,33 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SocialAuthController;
-
-Route::get('/', function () {
-    return view('welcome');
-    
-});
-// Route::middleware('guest')->group(function () {
-//     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-//     Route::post('/login', [AuthController::class, 'login']);
-//     Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-//     Route::post('/register', [AuthController::class, 'register']);
-// });
+ use App\Http\Controllers\DashboardController;
 Route::middleware('guest')->group(function () {
     Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-});
-Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-});
-Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
     Route::get('/password/reset', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
     Route::post('/password/email', [AuthController::class, 'sendResetLinkEmail'])->name('password.email');
 });
-use App\Http\Controllers\DashboardController;
 
 Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 // Google Authentication Routes
