a56878d5077ce05990221b3b0b518d37a7a8679b
tawbazf <tawbazehaf@gmail.com>
2025-04-22 10:48:16 +0100

    adding createing badge view in dashboardprof

--- codeclass/resources/views/dashboardProf.blade.php
+++ codeclass/resources/views/dashboardProf.blade.php
@@ -79,12 +79,12 @@
                     </svg>
                     Attribuer des Droits
                 </button>
-                <button class="btn-green py-2 px-4 rounded-md flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                    Créer un Badge
-                </button>
+                <a href="{{ route('badge.create') }}" class="btn-green py-2 px-4 rounded-md flex items-center">
+    <svg xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg") class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+    </svg>
+    Créer un Badge
+</a>
             </div>
             <div class="flex space-x-4 w-full sm:w-auto">
                 <div class="relative flex-grow max-w-xs">
