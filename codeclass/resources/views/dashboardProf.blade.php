6be5d4389430b417a56fbe9bf432282cb2fbd2eb
tawbazf <tawbazehaf@gmail.com>
2025-04-24 10:59:17 +0100

    feat: add link to view student skills on dashboard

--- codeclass/resources/views/dashboardProf.blade.php
+++ codeclass/resources/views/dashboardProf.blade.php
@@ -79,6 +79,12 @@
                     </svg>
                     Attribuer des Droits
                 </button>
+                <a href="{{ url('/skills/all') }}" class="btn-purple py-2 px-4 rounded-md flex items-center">
+        <svg xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg") class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 01.88 7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+        </svg>
+        Compétences des étudiants
+    </a>
                 <a href="{{ route('badge.create') }}" class="btn-green py-2 px-4 rounded-md flex items-center">
     <svg xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg") class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
