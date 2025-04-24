157e55c315d86d67d5ef24ef0abad5f318d9d1fc
tawbazf <tawbazehaf@gmail.com>
2025-04-24 16:44:09 +0100

    feat: update certifications view to display certification details; replace community references with certification data

--- codeclass/resources/views/certifications.blade.php
+++ codeclass/resources/views/certifications.blade.php
@@ -8,22 +8,20 @@
     </div>
 
     <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-    @foreach($communities as $community)
+    @forelse($certifications as $certification)
         <div class="bg-white shadow-md rounded-lg p-4">
-            <h2 class="text-xl font-semibold text-gray-800">{{ $community->title }}</h2>
-            <p class="text-gray-600 mt-2">{{ $community->description }}</p>
-            <a href="{{ $community->link }}" class="text-blue-500 hover:underline mt-4 inline-block">
-                {{ $community->link_text ?? 'Read More' }}
+            <h2 class="text-xl font-semibold text-gray-800">{{ $certification->title }}</h2>
+            <p class="text-gray-600 mt-2">{{ $certification->description }}</p>
+            <a href="{{ $certification->link }}" class="text-blue-500 hover:underline mt-4 inline-block">
+                {{ $certification->link_text ?? 'Read More' }}
             </a>
         </div>
-    @endforeach
-</section>
-        @empty
-            <div class="col-span-full py-10 text-center">
-                <p class="text-gray-500 text-lg">No certifications available at the moment.</p>
-            </div>
-        @endforelse
-    </div>
+    @empty
+        <div class="col-span-full py-10 text-center">
+            <p class="text-gray-500 text-lg">No certifications available at the moment.</p>
+        </div>
+    @endforelse
+    </section>
 
     @if($certifications->hasPages())
         <div class="mt-8">
