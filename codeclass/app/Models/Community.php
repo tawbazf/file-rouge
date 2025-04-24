715b2fb9120f78a477993f378a385977f824d5ee
tawbazf <tawbazehaf@gmail.com>
2025-04-24 10:14:01 +0100

    feat: define fillable attributes and user relationship in Community model

--- codeclass/app/Models/Community.php
+++ codeclass/app/Models/Community.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Community extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'message',
+    ];
+
+  
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
