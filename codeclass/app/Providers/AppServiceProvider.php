f8f812a180bff502b86fee7b6d1df41b04755c8d
tawbazf <tawbazehaf@gmail.com>
2025-05-02 21:41:54 +0100

    fix: update register method in AppServiceProvider to include JDoodleService registration

--- codeclass/app/Providers/AppServiceProvider.php
+++ codeclass/app/Providers/AppServiceProvider.php
@@ -9,9 +9,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        $this->app->singleton(JDoodleService::class, function ($app) {
+            return new JDoodleService();
+        });
     }
 
     /**
